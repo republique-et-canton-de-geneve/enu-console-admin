3a512410f5d125ffff2779a8e6c261d0c19afdc5
guillaume-mathe <guillaume.mathe@gmail.com>
2023-07-11 11:28:52 +0200

    Mise à jour des versions + ajout de Highcharts

--- app/src/components/Home.jsx
+++ app/src/components/Home.jsx
@@ -1,9 +1,26 @@
 import * as React from 'react';
 import {Typography} from "@mui/material";
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
+
+const options = {
+    title: {
+        text: 'My chart'
+    },
+    series: [{
+        data: [1, 2, 3]
+    }]
+}
 
 export default function Home() {
 
     return (
-        <Typography>Contenu de l'appli</Typography>
+        <>
+            <Typography>Contenu de l'appli</Typography>
+            <HighchartsReact
+                highcharts={Highcharts}
+                options={options}
+            />
+        </>
     )
 }
